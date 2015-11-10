8d526d1a0144f1a6208f0c2911f379a21b36b322
Matt Stjernholm <thew88@gmail.com>
2015-11-09 17:02:37 -0700

    oops, worked on master

--- test/integration/visitor_can_view_products_test.rb
+++ test/integration/visitor_can_view_products_test.rb
@@ -5,16 +5,13 @@ class VisitorCanViewProductsTest < ActionDispatch::IntegrationTest
   test 'a user can view items' do
     item = Item.create(name: "gnar possum",
                        description: "a snowboard for shredding gnar pow",
-                       price: 1000,
-                       image: "picture url")
+                       price: 1000)
     item = Item.create(name: "gwar possum",
                        description: "a snowboard for gwar concerts",
-                       price: 15,
-                       image: "gwar picture url")
+                       price: 15)
 
     visit items_path
 
-    save_and_open_page
     assert page.has_content?("Snowboards")
     assert page.has_content?("gnar possum")
     assert page.has_content?("gwar possum")
--- test/models/item_test.rb
+++ test/models/item_test.rb
@@ -1,7 +1,40 @@
 require 'test_helper'
 
 class ItemTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "we can create a new item with valid attributes" do
+    item = Item.create(name: "gnar possum",
+                       description: "a snowboard for shredding gnar pow",
+                       price: 1000,
+                       image_file_name: 'hi.png')
+
+    assert item.save
+  end
+
+  test "we cannot create a new item without a name" do
+    item = Item.create(description: "a snowboard for shredding gnar pow",
+                       price: 1000,
+                       image_file_name: 'hi.png')
+
+    refute item.save
+  end
+
+  test "we cannot create a new item without a price" do
+    item = Item.create(name: "gnar possum",
+                       description: "a snowboard for shredding gnar pow",
+                       image_file_name: 'hi.png')
+
+    refute item.save
+  end
+
+  test "we cannot create a new item with invalid attributes" do
+    item = Item.create(name: "gnar possum",
+                       description: "a snowboard for shredding gnar pow",
+                       price: "hey",
+                       image_file_name: 'hi.png')
+
+    refute item.save
+  end
+
+
+
 end
