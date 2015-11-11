5501e9ad97b0bce37559ea71db483bc21768b24d
Edgar Duran <edgarduran@Edgars-MacBook-Pro-2.local>
2015-11-11 12:58:40 -0700

    all tests working

--- test/integration/visitor_can_add_item_to_cart_and_view_cart_test.rb
+++ test/integration/visitor_can_add_item_to_cart_and_view_cart_test.rb
@@ -20,18 +20,16 @@ class VisitorCanAddItemToCartAndViewCartTest < ActionDispatch::IntegrationTest
   end
 
   test 'visitor can add item to cart from show page' do
-    skip
      Item.create(name: "gnar possum",
                  description: "a snowboard for shredding gnar pow",
                  price: 1000)
     visit items_path
-    click_button "VIEW ITEM"
+    click_link "View Item"
     click_link "Add To Cart"
 
-    within(".cart") do
-      assert page.has_content?(1)
-    end
+    assert page.has_content?("Cart: 1")
   end
+
   test 'visitor can view cart with selected items' do
     Item.create(name: "gnar possum",
                 description: "a snowboard for shredding gnar pow",
@@ -43,5 +41,37 @@ class VisitorCanAddItemToCartAndViewCartTest < ActionDispatch::IntegrationTest
     assert_equal current_path, cart_path
     assert page.has_content?("Gnar possum")
     assert page.has_content?("1000")
+  end
+
+  test 'visitor can view cart with multiple items' do
+    item_1 = Item.create(name: "gnar possum",
+                description: "a snowboard for shredding gnar pow",
+                price: 1000)
+    item_2 = Item.create(name: "gnar possum",
+                description: "a snowboard for shredding gnar pow",
+                price: 1000)
+    item_3 = Item.create(name: "Sweet Jacket",
+                description: "Keeps you warm",
+                price: 240)
+
+    visit items_path
+    within("#item_#{item_1.id}") do
+      click_link "Add To Cart"
+    end
+    within("#item_#{item_2.id}") do
+      click_link "Add To Cart"
+    end
+    within("#item_#{item_3.id}") do
+      click_link "Add To Cart"
+    end
+    click_link "Cart"
+    assert_equal current_path, cart_path
+    assert page.has_content?("Gnar possum")
+    assert page.has_content?("1000")
+    assert page.has_content?("Gnar possum")
+    assert page.has_content?("1000")
+    assert page.has_content?("2")
+    assert page.has_content?("Sweet jacket")
+    assert page.has_content?("240")
   end
 end
