fd365a234afb168a1206b72529c48b7f24f014fc
Matt Stjernholm <thew88@gmail.com>
2015-11-11 16:35:24 -0700

    add show pages for items
    -a user can click an item to view more details and add to cart

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :items, only: [:index]
+  resources :items, only: [:index, :show]
   resources :cart_items, only: [:create, :destroy]
   resources :categories, only: [:show, :index], param: :slug
 
