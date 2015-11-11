f3c05b84344830bf0a0c21022b8eb0d77205f2d8
Matt Rooney <matthewjrooney@gmail.com>
2015-11-11 11:20:45 -0700

    fix hound issues in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,27 +6,30 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-categories = Category.create([{title: "Apparel"},{title: "Snowboards"}])
+categories = Category.create( [ { title: "Apparel" },{ title: "Snowboards" } ] )
 
-apparel = Category.first.items.create([{name: "Hoody",
-                      description: "Keeps you slightly warm",
-                      price: 80,
-                      image: File.open('app/assets/images/hoody.jpg')},
+apparel = Category.first.items.create( [
+                  { name: "Hoody",
+                    description: "Keeps you slightly warm",
+                    price: 80,
+                    image: File.open("app/assets/images/hoody.jpg") },
 
-                     {name: "Sweet Jacket",
-                      description: "Keeps you warm",
-                      price: 240,
-                      image: File.open('app/assets/images/jacket.jpg')}])
-
-
-snowboards = Category.last.items.create([
-                    {name: "gwar possum",
-                     description: "a snowboard for gwar concerts",
-                     price: 15,
-                     image: File.open('app/assets/images/gwar_possum.jpg')},
+                  { name: "Sweet Jacket",
+                    description: "Keeps you warm",
+                    price: 240,
+                    image: File.open("app/assets/images/jacket.jpg") } ] )
 
 
-                     {name: "gnar possum",
-                      description: "a snowboard for shredding gnar pow",
-                      price: 1000,
-                      image: File.open('app/assets/images/gnar_possum.jpg')}])
+snowboards = Category.last.items.create( [
+                      { name: "gwar possum",
+                        description: "a snowboard for gwar concerts",
+                        price: 15,
+                        image: File.open("app/assets/images/gwar_possum.jpg")
+                      },
+
+
+                      { name: "gnar possum",
+                        description: "a snowboard for shredding gnar pow",
+                        price: 1000,
+                        image: File.open("app/assets/images/gnar_possum.jpg")
+                      } ] )
