02caec38a191daafe7550de686a4bb81eea74de6
Edgar Duran <edgarduran@Edgars-MacBook-Pro-2.local>
2015-11-11 17:46:01 -0700

    quantity selection works
    -test passing for quantity update
    -update method good to go

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :items, only: [:index]
-  resources :cart_items, only: [:create, :destroy]
+  resources :cart_items, only: [:create, :destroy, :update]
   resources :categories, only: [:show, :index], param: :slug
 
   get '/cart', to: "cart#show"
---
+++ test/integration/a_cart_items_quantity_can_be_updated_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class ACartItemsQuantityCanBeUpdatedTest < ActionDispatch::IntegrationTest
+  test 'cart items quantity can be updated' do
+    Item.create(name: "gnar possum",
+                description: "a snowboard for shredding gnar pow",
+                price: 1000)
+
+    visit items_path
+    click_link "Add To Cart"
+    click_link "Cart"
+    assert_equal current_path, cart_path
+
+    fill_in "Quantity", with: 3
+    click_button "Update Quantity"
+    assert page.has_content?("Gnar possum")
+    assert page.has_content?("3000")
+  end
+end
