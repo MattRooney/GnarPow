ce6d22ff0e069c5c225381ec56d00ccf2f9d886d
Matt Stjernholm <thew88@gmail.com>
2015-11-11 18:41:52 -0700

    add flash message to item removal from cart

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -11,8 +11,10 @@ class CartItemsController < ApplicationController
   end
 
   def destroy
-    @cart.contents.delete_if { |key, value| key == params[:id] }
-
+    item = Item.find(params[:id].to_i)
+    if @cart.contents.delete_if { |item_id, quantity| item_id == params[:id] }
+      flash[:remove] = "You have removed the item #{view_context.link_to(item.name, item_path(item))} from your cart."
+    end
     redirect_to cart_path
   end
 
