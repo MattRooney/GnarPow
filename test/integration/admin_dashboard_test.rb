99f6d55885874e214991e68389b703d8e99b2238
Edgar Duran <edgarduran@Edgars-MacBook-Pro-2.local>
2015-11-13 16:22:32 -0700

    fixed hound issues

--- test/integration/admin_dashboard_test.rb
+++ test/integration/admin_dashboard_test.rb
@@ -1,7 +1,6 @@
-require "test_helper"
+require 'test_helper'
 
 class AdminDashboardTest < ActionDispatch::IntegrationTest
-
   test 'logged in admin sees dashboard page' do
     admin = User.create(username: "admin",
                         password: "password",
@@ -14,12 +13,11 @@ class AdminDashboardTest < ActionDispatch::IntegrationTest
   end
   test 'default user does not see admin dashboard' do
     user = User.create(username: "default_user",
-                        password: "password",
-                        role: 0)
+                       password: "password",
+                       role: 0)
 
     ApplicationController.any_instance.stubs(:current_user).returns(user)
     visit admin_dashboard_path(user)
-    save_and_open_page
 
     refute page.has_content?("Welcome, Default_user!")
     assert page.has_content?("The page you were looking for doesn't exist.")
