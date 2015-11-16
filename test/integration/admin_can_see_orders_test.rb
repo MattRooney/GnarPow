362dd0f6aaf8d0640f473cfbd540c37700654b7e
Edgar Duran <edgarduran@Edgars-MacBook-Pro-2.local>
2015-11-15 19:25:02 -0700

    orders status counter works

--- test/integration/admin_can_see_orders_test.rb
+++ test/integration/admin_can_see_orders_test.rb
@@ -12,9 +12,30 @@ class AdminCanSeeOrdersTest < ActionDispatch::IntegrationTest
     fill_in "Username", with: "admin"
     fill_in "Password", with: "password"
     click_button "Login"
-    save_and_open_page
+
     assert page.has_content?("Welcome, Admin!")
-    assert page.has_content?("Orders")
-    assert page.has_content?("Status")
+    within('.all-orders') do
+      assert page.has_content?("Status")
+      assert page.has_content?("Total Price")
+      assert page.has_content?("$160")
+      assert page.has_link?("View Order")
+    end
+  end
+  test 'admin sees order numbers by status' do
+    admin = User.create(username: "admin",
+                        password: "password",
+                        role:      1)
+    create_items_associated_with_orders
+    ApplicationController.any_instance.stubs(:current_user).returns(admin)
+    visit login_path
+    fill_in "Username", with: "admin"
+    fill_in "Password", with: "password"
+    click_button "Login"
+    within('.by-status') do
+      assert page.has_content?("Ordered")
+      assert page.has_content?("Cancelled")
+      assert page.has_content?("Completed")
+      assert page.has_content?("2")
+    end
   end
 end
