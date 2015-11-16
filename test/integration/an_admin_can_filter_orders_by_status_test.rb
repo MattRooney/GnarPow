f5293b1433c66420cf873f593eeaeef306fb5085
Matt Stjernholm <thew88@gmail.com>
2015-11-16 15:11:50 -0700

    make admin order filtering tests more robust

--- test/integration/an_admin_can_filter_orders_by_status_test.rb
+++ test/integration/an_admin_can_filter_orders_by_status_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class AnAdminCanFilterOrdersByStatusTest < ActionDispatch::IntegrationTest
   include CategoryItemsSetup
-  test 'the truth' do
+  test 'admin can view paid orders' do
     admin = User.create(username: 'admin',
                         password: 'password',
                         role:      1)
@@ -12,7 +12,81 @@ class AnAdminCanFilterOrdersByStatusTest < ActionDispatch::IntegrationTest
     ApplicationController.any_instance.stubs(:current_user).returns(admin)
 
     visit admin_dashboard_index_path(admin)
+
+    click_link('View Paid')
+
+    assert_equal admin_dashboard_path('paid'), current_path
+
+    within('.orders') do
+      assert page.has_content?("Placed On")
+      assert page.has_content?("Status")
+      assert page.has_content?("paid")
+    end
+  end
+
+  test 'admin can view canceled orders' do
+    admin = User.create(username: 'admin',
+                        password: 'password',
+                        role:      1)
+
+    create_items_associated_with_orders
+
+    ApplicationController.any_instance.stubs(:current_user).returns(admin)
+
+    visit admin_dashboard_index_path(admin)
+
+    click_link('View Canceled')
+
+    assert_equal admin_dashboard_path('canceled'), current_path
+
+    within('.orders') do
+      assert page.has_content?("Placed On")
+      assert page.has_content?("Status")
+      assert page.has_content?("canceled")
+    end
+  end
+
+  test 'admin can view completed orders' do
+    admin = User.create(username: 'admin',
+                        password: 'password',
+                        role:      1)
+
+    create_items_associated_with_orders
+
+    ApplicationController.any_instance.stubs(:current_user).returns(admin)
+
+    visit admin_dashboard_index_path(admin)
+
+    click_link('View Completed')
+
+    assert_equal admin_dashboard_path('completed'), current_path
+
+    within('.orders') do
+      assert page.has_content?("Placed On")
+      assert page.has_content?("Status")
+      assert page.has_content?("completed")
+    end
+  end
+
+  test 'admin can view ordered orders' do
+    admin = User.create(username: 'admin',
+                        password: 'password',
+                        role:      1)
+
+    create_items_associated_with_orders
+
+    ApplicationController.any_instance.stubs(:current_user).returns(admin)
+
+    visit admin_dashboard_index_path(admin)
+
     click_link('View Ordered')
-    save_and_open_page
+
+    assert_equal admin_dashboard_path('ordered'), current_path
+
+    within('.orders') do
+      assert page.has_content?("Placed On")
+      assert page.has_content?("Status")
+      assert page.has_content?("ordered")
+    end
   end
 end
