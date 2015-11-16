6e68056560cb4932134a86f652db9c6606e8240d
Matt Stjernholm <thew88@gmail.com>
2015-11-16 15:00:41 -0700

    rubocop these things

--- test/integration/an_admin_can_filter_orders_by_status_test.rb
+++ test/integration/an_admin_can_filter_orders_by_status_test.rb
@@ -2,18 +2,17 @@ require 'test_helper'
 
 class AnAdminCanFilterOrdersByStatusTest < ActionDispatch::IntegrationTest
   include CategoryItemsSetup
-  test "the truth" do
+  test 'the truth' do
     admin = User.create(username: 'admin',
                         password: 'password',
                         role:      1)
 
     create_items_associated_with_orders
 
-
     ApplicationController.any_instance.stubs(:current_user).returns(admin)
 
     visit admin_dashboard_index_path(admin)
-    click_link("View Ordered")
+    click_link('View Ordered')
     save_and_open_page
   end
 end
