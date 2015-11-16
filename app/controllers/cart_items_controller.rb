b973657e8f3df7eeb78b6bf3f64d466e245fb88f
Matt Stjernholm <thew88@gmail.com>
2015-11-15 21:09:02 -0700

    restyle item show page

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -5,7 +5,7 @@ class CartItemsController < ApplicationController
     item = Item.find(params[:item_id])
     @cart.add_item(item.id)
     session[:cart] = @cart.contents
-    flash[:notice] = "You now have #{pluralize(@cart.count_of(item.id), item.name)} in your cart."
+    flash[:notice] = "You now `have` #{pluralize(@cart.count_of(item.id), item.name)} in your cart."
 
     redirect_to items_path
   end
@@ -22,7 +22,7 @@ class CartItemsController < ApplicationController
   def destroy
     item = Item.find(params[:id].to_i)
     if @cart.remove_items(params)
-      flash[:remove] = "You have removed the item #{view_context.link_to(item.name, item_path(item))} from your cart."
+      flash[:remove] = "You have removed the item  #{view_context.link_to(item.name, item_path(item))} from your cart."
     end
     redirect_to cart_path
   end
