9cc3f26f65b8fd0d98ac485c114ccecb7575cadb
Matt Rooney <matthewjrooney@gmail.com>
2015-11-16 17:23:44 -0700

    implement sample email
    - sample email sends on order creation
    - currently hard-coded to: address

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,7 @@ Rails.application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -49,4 +49,6 @@ Rails.application.configure do
     authentication: 'plain',
     enable_starttls_auto: true
   }
+
+  config.action_mailer.default_url_options { host: "localhost:3000" }
 end
