134e4feed31d9199e7db9ee2a17f88ac6ecb80f3
Edgar Duran <edgarduran@Edgars-MacBook-Pro-2.local>
2015-11-16 22:39:11 -0700

    create method

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,4 +10,20 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to admin_dashboard_index_path
+      flash[:success] = "Logged in as #{@user.username}"
+    else
+      flash.now[:error] = 'Please fill in required fields'
+      render :new
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :description, :brand, :price, :category_id)
+  end
 end
--- test/integration/admin_can_create_store_items_test.rb
+++ test/integration/admin_can_create_store_items_test.rb
@@ -2,6 +2,7 @@ require 'test_helper'
 
 class AdminCanCreateStoreItemsTest < ActionDispatch::IntegrationTest
   test 'admin can modify their account data' do
+    @category = Category.create(title: 'Apparel')
     admin = User.create(username: 'admin',
                         password: 'password',
                         role:      1)
@@ -13,7 +14,7 @@ class AdminCanCreateStoreItemsTest < ActionDispatch::IntegrationTest
     fill_in 'Name', with: 'NewGear'
     fill_in 'Description', with: 'Put this thing on!'
     fill_in 'Price', with: '777'
-    fill_in 'Category', with: 'Apparel'
+    fill_in 'Category', with: '1'
     click_button 'Create Item'
     assert page.has_content?('Welcome, Newname!')
   end
