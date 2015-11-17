1fca47a0d74bee406860083175722a74d02217b1
Edgar Duran <edgarduran@Edgars-MacBook-Pro-2.local>
2015-11-16 22:00:15 -0700

    add form for new item

---
+++ test/integration/admin_can_create_store_items_test.rb
@@ -0,0 +1,20 @@
+require 'test_helper'
+
+class AdminCanCreateStoreItemsTest < ActionDispatch::IntegrationTest
+  test 'admin can modify their account data' do
+    admin = User.create(username: 'admin',
+                        password: 'password',
+                        role:      1)
+
+    ApplicationController.any_instance.stubs(:current_user).returns(admin)
+    visit admin_dashboard_index_path(admin)
+    click_link 'Add items to store'
+
+    fill_in 'Name', with: 'NewGear'
+    fill_in 'Description', with: 'Put this thing on!'
+    fill_in 'Price', with: '777'
+    fill_in 'Category', with: 'Apparel'
+    click_button 'Create Item'
+    assert page.has_content?('Welcome, Newname!')
+  end
+end
