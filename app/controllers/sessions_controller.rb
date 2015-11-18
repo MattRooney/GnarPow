5a40680ec457a1dd487258c8d3dd5bfae3426c7f
Matt Stjernholm <thew88@gmail.com>
2015-11-17 21:34:53 -0700

    shorten the update cart quantiti method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,6 @@
 class SessionsController < ApplicationController
+  before_action
+
   def new
   end
 
@@ -7,7 +9,7 @@ class SessionsController < ApplicationController
     if @user && @user.authenticate(params[:session][:password])
       session[:user_id] = @user.id
       if current_admin?
-        redirect_to admin_dashboard_index_path(@user)
+        redirect_to admin_dashboard_index_path
         flash[:success] = "Logged in as #{@user.username}"
       else
         redirect_to @user
