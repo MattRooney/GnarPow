88f9900279a0c11b9ae977b04ece8aaa2fd9bd75
Matt Rooney <matthewjrooney@gmail.com>
2015-11-17 18:55:06 -0700

    fix typo in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { :host => "gnarpow.herokuapp.com"}
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :smtp
-  confing.action_mailer.perform_deliveries = true
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
