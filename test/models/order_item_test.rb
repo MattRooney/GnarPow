6009ed07bd740eb541b64d73ffa56cce6287282a
Edgar Duran <edgarduran@Edgars-MacBook-Pro-2.local>
2015-11-19 10:48:34 -0700

    clean up tests add order item test

--- test/models/order_item_test.rb
+++ test/models/order_item_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class OrderItemTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  include CategoryItemsSetup
+  test 'items are asscociated wit an order' do
+    create_items
+    order = Order.new(current_status: 'completed')
+    order_item = OrderItem.new(item_id: Item.last.id, order_id: order.id, quantity: 2)
+    order.order_items << order_item
+    order.save
+
+    assert order.valid?
+    assert order_item.valid?
+  end
 end
